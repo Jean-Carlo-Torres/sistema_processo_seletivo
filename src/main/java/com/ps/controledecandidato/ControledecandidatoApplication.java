7e2bb45639f0234694feb04cc712a8dfdbb41cf3
Jean Carlo <jeansanto2002@gmail.com>
2024-07-27 13:44:36 -0300

    Documentando a API com o Swagger

--- src/main/java/com/ps/controledecandidato/ControledecandidatoApplication.java
+++ src/main/java/com/ps/controledecandidato/ControledecandidatoApplication.java
@@ -1,13 +1,22 @@
 package com.ps.controledecandidato;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Swagger OpenApi",
+                version = "1",
+                description = "API Web para controle de candidatos em processo seletivo")
+)
 public class ControledecandidatoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ControledecandidatoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ControledecandidatoApplication.class, args);
+    }
 
 }
--- src/main/java/com/ps/controledecandidato/controller/CandidatoController.java
+++ src/main/java/com/ps/controledecandidato/controller/CandidatoController.java
@@ -2,6 +2,10 @@ package com.ps.controledecandidato.controller;
 
 import com.ps.controledecandidato.entities.Candidato;
 import com.ps.controledecandidato.repository.CandidatoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,26 +15,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/candidatos")
+@Tag(name = "candidatos")
 public class CandidatoController {
 
     @Autowired
     private CandidatoRepository repository;
 
-    @PostMapping
-    public Candidato criarCandidato(@RequestBody @Valid Candidato candidato) {
-        return repository.save(candidato);
-    }
-
+    @Operation(summary = "Obter todos os candidatos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de todos os candidatos"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida")
+    })
     @GetMapping
     public Iterable<Candidato> obterCandidatos() {
         return repository.findAll();
     }
 
+    @Operation(summary = "Obter um candidato por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Candidato encontrado"),
+            @ApiResponse(responseCode = "404", description = "Candidato não encontrado")
+    })
     @GetMapping("/{id}")
     public Candidato obterCandidatoPorId(@PathVariable Long id) {
         return repository.findById(id).orElse(null);
     }
 
+    @Operation(summary = "Obter candidatos selecionados para entrevista")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de candidatos selecionados"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida")
+    })
     @GetMapping("/candidatosselecionados")
     public Iterable<Candidato> obterCandidatosSelecionados() {
         List<Candidato> candidatosSelecionados = new ArrayList<>();
@@ -47,24 +62,33 @@ public class CandidatoController {
         return candidatosSelecionados;
     }
 
-
+    @Operation(summary = "Atualizar um candidato existente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Candidato atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Candidato não encontrado")
+    })
     @PutMapping("/{id}")
     public Candidato atualizarCandidato(@PathVariable Long id, @RequestBody @Valid Candidato candidatoAtualizado) {
-        Candidato candidatoExistente = repository.findById(id).orElse(null);
-        if (candidatoExistente != null) {
-            candidatoExistente.setNome(candidatoAtualizado.getNome());
-            candidatoExistente.setEmail(candidatoAtualizado.getEmail());
-            candidatoExistente.setTelefone(candidatoAtualizado.getTelefone());
-            candidatoExistente.setExperiencia(candidatoAtualizado.getExperiencia());
-            candidatoExistente.setEscolaridade(candidatoAtualizado.getEscolaridade());
-            candidatoExistente.setGenero(candidatoAtualizado.getGenero());
-            candidatoExistente.setDataNascimento(candidatoAtualizado.getDataNascimento());
-            candidatoExistente.setPretensaoSalarial(candidatoAtualizado.getPretensaoSalarial());
-            return repository.save(candidatoExistente);
-        }
-        return null;
+        return repository.findById(id)
+                .map(candidatoExistente -> {
+                    candidatoExistente.setNome(candidatoAtualizado.getNome());
+                    candidatoExistente.setEmail(candidatoAtualizado.getEmail());
+                    candidatoExistente.setTelefone(candidatoAtualizado.getTelefone());
+                    candidatoExistente.setExperiencia(candidatoAtualizado.getExperiencia());
+                    candidatoExistente.setEscolaridade(candidatoAtualizado.getEscolaridade());
+                    candidatoExistente.setGenero(candidatoAtualizado.getGenero());
+                    candidatoExistente.setDataNascimento(candidatoAtualizado.getDataNascimento());
+                    candidatoExistente.setPretensaoSalarial(candidatoAtualizado.getPretensaoSalarial());
+                    return repository.save(candidatoExistente);
+                })
+                .orElse(null);
     }
 
+    @Operation(summary = "Deletar um candidato existente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Candidato deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Candidato não encontrado")
+    })
     @DeleteMapping("/{id}")
     public void deletarCandidato(@PathVariable Long id) {
         repository.deleteById(id);
